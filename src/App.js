7d2aa77b4827682fa4be803aabb8a19af8307fd3
tomasbond02 <bondtomy@gmail.com>
2024-01-22 17:14:03 -0300

    otro commando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeaderComponent from './components/headerComponent';
+import MainComponent from './components/mainComponent';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent/>
+      <MainComponent/>
     </div>
   );
 }
--- src/components/headerComponent/index.js
+++ src/components/headerComponent/index.js
@@ -8,19 +8,19 @@ function HeaderComponent() {
     <header className="App-header">
       <img src="logoColinas.png" alt="logoImagen" />
       <div>
-        <Link to="/inicio" className="nav-link">
+        <Link className="active-link">
           {'INICIO'}
         </Link>
-        <Link to="/servicios" className="nav-link">
+        <Link  className="nav-link">
           {'SERVICIOS'}
         </Link>
-        <Link to="/pax4" className="nav-link">
+        <Link className="nav-link">
           {'PAX 4'}
         </Link>
-        <Link to="/pax6" className="nav-link">
+        <Link className="nav-link">
           {'PAX 6'}
         </Link>
-        <Link to="/reserve" className="nav-link">
+        <Link className="nav-link">
           {'RESERVE'}
         </Link>
       </div>
